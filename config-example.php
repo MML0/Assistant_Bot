39b9fe45f8e2e342080ff9ac6b3628d8ab6d5023
MML0 <h1337124lat@gmail.com>
2025-11-14 21:39:09 +0330

    first commit

---
+++ config-example.php
@@ -0,0 +1,14 @@
+<?php
+// Configuration settings config-example.php 
+return [
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'testdb',
+        'username' => 'root',
+        'password' => '',
+    ],
+    'admin' => [
+        'password' => 'admin123456!@#$%^6',
+    ],
+];
+?>
